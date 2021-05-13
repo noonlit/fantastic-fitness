614621ef93f05f5e08919f328a9fc166ad3f8af6
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-05-13 16:32:09 +0300

    Add Activity VIew Model.

---
+++ ffitness/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ffitness.ViewModels
+{
+    public class ActivityViewModel
+    {
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int DifficultyLevel { get; set; }
+
+        public String Colour { get; set; }
+    }
+}
+}
