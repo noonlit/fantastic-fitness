a1dac006f7486caa03ceee1cf9c763c3e8a8be88
noonlit <julietfeynman@gmail.com>
2021-05-17 12:58:16 +0300

    Add description to scheduled activity

--- ffitness/Migrations/20210517095737_AddDescriptionToScheduledActivity.Designer.cs
+++ ffitness/Migrations/20210517095737_AddDescriptionToScheduledActivity.Designer.cs
@@ -4,14 +4,16 @@ using Ffitness.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ffitness.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210517095737_AddDescriptionToScheduledActivity")]
+    partial class AddDescriptionToScheduledActivity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
