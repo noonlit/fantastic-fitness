385bf8367f4b6da8804d81c4f00159646bc6ad71
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-05-23 11:45:38 +0300

    Add SecondaryColour, Description, ActivityType enum to Activity. Modify ActivityViewModel.

--- ffitness/Migrations/20210523082618_AddTypeEnumAndDescriptionToActivity.Designer.cs
+++ ffitness/Migrations/20210523082618_AddTypeEnumAndDescriptionToActivity.Designer.cs
@@ -4,14 +4,16 @@ using Ffitness.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ffitness.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210523082618_AddTypeEnumAndDescriptionToActivity")]
+    partial class AddTypeEnumAndDescriptionToActivity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -41,6 +43,10 @@ namespace Ffitness.Migrations
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+                    b.Property<string>("Colour")
+                        .HasMaxLength(7)
+                        .HasColumnType("nvarchar(7)");
+
                     b.Property<string>("Description")
                         .HasColumnType("nvarchar(max)");
 
@@ -50,10 +56,6 @@ namespace Ffitness.Migrations
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PrimaryColour")
-                        .HasMaxLength(7)
-                        .HasColumnType("nvarchar(7)");
 
                     b.Property<string>("SecondaryColour")
                         .HasMaxLength(7)
