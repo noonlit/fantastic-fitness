45685dc1cb934eedc346bc720a46e2471869c761
Amalia Vargatu <avargatu@yahoo.com>
2021-05-30 08:10:25 +0300

    add subscription view model an controller

---
+++ ffitness/Controllers/SubscriptionsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ffitness.Data;
+using Ffitness.Models;
+using AutoMapper;
+using Ffitness.ViewModels;
+
+namespace Ffitness.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public SubscriptionsController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+
+        }
+
+        // GET: api/Subscriptions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubscriptionViewModel>>> GetSubscriptions()
+        {
+            return await _context.Subscriptions
+                .Select(a => _mapper.Map<SubscriptionViewModel>(a)).ToListAsync();
+        }
+
+        // GET: api/Subscriptions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subscription>> GetSubscription(int id)
+        {
+            var subscription = await _context.Subscriptions.FindAsync(id);
+
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            return subscription;
+        }
+
+        // PUT: api/Subscriptions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubscription(int id, Subscription subscription)
+        {
+            if (id != subscription.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subscription).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubscriptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subscriptions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Subscription>> PostSubscription(Subscription subscription)
+        {
+            _context.Subscriptions.Add(subscription);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubscription", new { id = subscription.Id }, subscription);
+        }
+
+        // DELETE: api/Subscriptions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubscription(int id)
+        {
+            var subscription = await _context.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subscriptions.Remove(subscription);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SubscriptionExists(int id)
+        {
+            return _context.Subscriptions.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ffitness/ViewModels/SubscriptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Ffitness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ffitness.ViewModels
+{
+    public class SubscriptionViewModel
+    {
+        public int Id { get; set; }
+        public ApplicationUser User { get; set; }
+        public SubscriptionDuration Duration { get; set; }
+        public double SubscriptionPrice { get; set; }
+        public string UserId { get; set; }
+    }
+}
