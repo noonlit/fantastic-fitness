5650b4874b80a1bc57177950d6d419b802fc67b7
noonlit <julietfeynman@gmail.com>
2021-06-27 11:36:33 +0300

    Update bookings-stats.component.ts

--- ffitness/ClientApp/src/app/bookings/admin/bookings-stats.component.ts
+++ ffitness/ClientApp/src/app/bookings/admin/bookings-stats.component.ts
@@ -21,7 +21,7 @@ export class BookingsStatsComponent implements OnInit {
 
   public colours = [
     {
-      backgroundColor: 'rgba(104,176,171,0.2)'
+      backgroundColor: 'rgba(155, 196, 188,0.7)'
     }
   ]
 
