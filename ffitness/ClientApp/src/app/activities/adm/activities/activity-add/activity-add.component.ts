9897151ae586f7a5d43409c7289f0b357d1f9c5b
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-06-28 18:28:27 +0300

    Update add activity component html and ts.

--- ffitness/ClientApp/src/app/activities/adm/activities/activity-add/activity-add.component.ts
+++ ffitness/ClientApp/src/app/activities/adm/activities/activity-add/activity-add.component.ts
@@ -9,7 +9,6 @@ import { ActivityComponentService } from '../../../shared/activity.service';
 export class AdminActivityAddComponent implements OnInit {
   @Input() activity: Activity = new Activity();
   ACTIVITY_TYPES = ACTIVITY_TYPES;
-  //activities: Activity[];
   message: string;
   errorMessages: [];
 
@@ -18,14 +17,7 @@ export class AdminActivityAddComponent implements OnInit {
     private cd: ChangeDetectorRef,
   ) { }
 
-  ngOnInit(): void {
-    /*
-    this.service.getActivities()
-      .subscribe(result => {
-        this.activities = result;
-      }, error => console.error(error));
-      */
-  }
+  ngOnInit(): void { }
 
   saveActivity() {
     this.service.save(this.activity)
